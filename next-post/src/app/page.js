43207300e8bbe2342437089bc3fff51da9484549
satyam <satyamkale271@gmail.com>
2025-01-13 21:30:10 +0530

    navigation router

--- next-post/src/app/page.js
+++ next-post/src/app/page.js
@@ -1,7 +1,19 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const HandleNavigation = () => {
+    router.push("/posts");
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Home Page</h1>
+      <button onClick={HandleNavigation} className="p-2  bg-blue-600 ">
+        Go to Posts
+      </button>
+    </div>
   );
 }
