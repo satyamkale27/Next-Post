1aaef2acb319599e6cf70ff9c4cc0dd3f4bdf964
satyam <satyamkale271@gmail.com>
2025-01-13 22:13:32 +0530

    dynamic routes and query push

--- next-post/src/app/page.js
+++ next-post/src/app/page.js
@@ -8,11 +8,18 @@ export default function Home() {
     router.push("/posts");
   };
 
+  const Pushquery = () => {
+    router.push("/posts/27");
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Home Page</h1>
       <button onClick={HandleNavigation} className="p-2  bg-blue-600 ">
         Go to Posts
+      </button>
+      <button onClick={Pushquery} className="p-2  bg-blue-600 ">
+        Go to Post no
       </button>
     </div>
   );
