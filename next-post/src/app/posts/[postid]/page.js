69b17db9b296413617ac38ff28472e1883b04d0a
Ayush Dixit <fsayush100@gmail.com>
2025-01-13 22:19:03 +0530

    add middleware file,server actions and way to get params at server in dynamic route

--- next-post/src/app/posts/[postid]/page.js
+++ next-post/src/app/posts/[postid]/page.js
@@ -1,11 +1,35 @@
-"use client";
+//client way
+
+// "use client";
+// import React from "react";
+// import { useParams } from "next/navigation";
+
+// const Page = () => {
+//   const { postid } = useParams();
+
+//   console.log(postid,"postid")
+//   return <div>{postid}</div>;
+// };
+
+// export default Page;
+
+// server way to do same
+import { serverAction } from "@/actions/example";
 import React from "react";
-import { useParams } from "next/navigation";
 
-const Page = () => {
-  const params = useParams();
-  const { postid } = useParams();
-  return <div>{postid}</div>;
-};
-
-export default Page;
+export default async function Page({ params }) {
+  const fetchData = await serverAction();
+  // fetchData is fetched in server side
+  const { postid } = await params;
+  console.log(postid, "postid");
+  return (
+    <div>
+      <div>postid={postid} </div>
+      <div>
+        {fetchData.map((d) => (
+          <div>{d.id}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
